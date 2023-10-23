f18e926902ae3988243004317b60fcd6c750624e
yangjiefeng <yangjiefeng@gmail.com>
2023-10-23 11:07:06 +0800

    update docs

--- docs/drafts/UUIDTest.java
+++ docs/drafts/UUIDTest.java
@@ -5,6 +5,21 @@ import java.math.BigInteger;
 public class UUIDTest {
 
     public static void main(String[] args) {
+        String s1 = nextId();
+        String s2 = nextId();
+        // 使用 compareTo() 方法比较两个字符串
+        int result = s1.compareTo(s2);
+        // 根据结果判断两个字符串的关系
+        if (result == 0) {
+            System.out.println("s1 and s2 are equal");
+        } else if (result < 0) {
+            System.out.println("s1 is smaller than s2");
+        } else {
+            System.out.println("s1 is greater than s2");
+        }
+    }
+
+    private static String nextId() {
         // Generate a random UUID
         UUID uuid = UUID.randomUUID();
         // Print the UUID
@@ -37,5 +52,6 @@ public class UUIDTest {
         String compressedBase64 = Base64.getEncoder().encodeToString(bytes);
         // Print the compressed Base64 string
         System.out.println("Compressed Base64: " + compressedBase64);
+        return compressedBase64.replace("O", "-"); // replace the alphabet "O" with "-"
     }
 }
